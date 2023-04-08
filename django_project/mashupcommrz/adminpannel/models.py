9d0b53a4f657c19d8af65fe30876e8628fe97da1
Abhi <bhnadh@gmail.com>
2023-04-08 23:04:33 +0530

    Django Project

---
+++ django_project/mashupcommrz/adminpannel/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Products(models.Model):
+    product_name = models.CharField(max_length=200)
+    price = models.FloatField()
+    product_description = models.CharField(max_length=1000)
+    product_picture = models.FileField()
+    is_active = models.SmallIntegerField(default = 1) 
